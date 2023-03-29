355eeb63189409b3cfe20dfa077055fb81517523
li231sd <113144462+li231sd@users.noreply.github.com>
2023-03-28 17:31:57 -0700

    UPDATE! BETA 3.0!
    LOG:
    ADD TEXT BUTTON
    NEW CROSSHAIR
    BIGGER BOARD
    BUG FIXES
    AND MORE!

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const addTextButton = document.getElementById("addTextButton");
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let textX = 0;
+let textY = 0;
+
+addTextButton.addEventListener("click", addText);
 
 //PC
 canvas.addEventListener("mousedown", (e) => {
@@ -27,6 +32,27 @@ canvas.addEventListener("mousemove", (e) => {
 canvas.addEventListener("mouseup", () => {
   isDrawing = false;
 });
+
+function addText() {
+  const text = prompt("Before adding text please click or tap somewhere on the board where you want to add your text:");
+
+  ctx.font = "25px Arial";
+  ctx.fillStyle = document.getElementById("color-picker").value;
+  const textX = lastX;
+  const textY = lastY;
+
+  const textWidth = ctx.measureText(text).width;
+  const textHeight = parseInt(ctx.font);
+
+  if (textX + textWidth > canvas.width) {
+    textX = canvas.width - textWidth;
+  }
+  if (textY + textHeight > canvas.height) {
+    textY = canvas.height - textHeight;
+  }
+
+  ctx.fillText(text, textX, textY);
+}
 
 //MOBILE
 canvas.addEventListener("touchstart", (e) => {
@@ -52,15 +78,46 @@ canvas.addEventListener("touchend", () => {
   isDrawing = false;
 });
 
+function addText() {
+  const text = prompt("Before adding text please click or tap somewhere on the board where you want to add your text:");
+
+  if (isTouchDevice()) {
+    textX = lastX - canvas.offsetLeft;
+    textY = lastY - canvas.offsetTop;
+  } else {
+    textX = lastX;
+    textY = lastY;
+  }
+
+  ctx.font = "25px Arial";
+  ctx.fillStyle = document.getElementById("color-picker").value;
+  
+  const textWidth = ctx.measureText(text).width;
+  const textHeight = parseInt(ctx.font);
+
+  if (textX + textWidth > canvas.width) {
+    textX = canvas.width - textWidth;
+  }
+  if (textY + textHeight > canvas.height) {
+    textY = canvas.height - textHeight;
+  }
+
+  ctx.fillText(text, textX, textY);
+}
+
+function isTouchDevice() {
+  return 'ontouchstart' in window || navigator.maxTouchPoints > 0; 
+}
+
 //UNIVERSAL 
 document.getElementById("clear-button").addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
-  
+
 document.getElementById("save-button").addEventListener("click", () => {
-    const image = canvas.toDataURL("image/png");
-    const link = document.createElement("a");
-    link.download = "drawing.png";
-    link.href = image;
-    link.click();
+  const image = canvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.download = "drawing.png";
+  link.href = image;
+  link.click();
 });
