b239221388bd04280b91c1d764bd9b96e3fc0646
li231sd <113144462+li231sd@users.noreply.github.com>
2023-03-31 14:21:29 -0700

    BETA 6.8!

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const releaseNotes = document.getElementById('release-notes');
 const initialBackgroundColor = "#ffffff";
 
 var overlay = document.getElementById("overlay");
+var audioPlayer = document.getElementById("audio-player");
+var playButton = document.getElementById("play-button");
+
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
@@ -20,6 +23,20 @@ let isCircleDrawn = true;
 let currentBackgroundColor = initialBackgroundColor;
 
 addTextButton.addEventListener("click", addText);
+audioPlayer.volume = 0.2;
+
+playButton.addEventListener("click", function() {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+  } else {
+    audioPlayer.pause();
+  }
+});
+
+audioPlayer.addEventListener("ended", function() {
+  audioPlayer.currentTime = 0;
+  audioPlayer.play();
+});
 
 function resetCanvasBackground() {
   currentBackgroundColor = initialBackgroundColor;
@@ -89,37 +106,96 @@ canvas.addEventListener("mouseup", () => {
   isDrawing = false;
 });
 
+function openPrompt(promptText) {
+  const promptContainer = document.createElement('div');
+  promptContainer.classList.add('prompt-container');
+
+  const promptBox = document.createElement('div');
+  promptBox.classList.add('prompt-box');
+
+  const promptMessage = document.createElement('p');
+  promptMessage.classList.add('prompt-message');
+  promptMessage.innerText = promptText;
+
+  const promptInput = document.createElement('input');
+  promptInput.classList.add('prompt-input');
+  promptInput.setAttribute('type', 'text');
+
+  const submitButton = document.createElement('button');
+  submitButton.classList.add('prompt-submit-button');
+  submitButton.innerText = 'Submit';
+
+  const cancelButton = document.createElement('button');
+  cancelButton.classList.add('prompt-cancel-button');
+  cancelButton.innerText = 'Cancel';
+
+  promptBox.appendChild(promptMessage);
+  promptBox.appendChild(promptInput);
+  promptBox.appendChild(submitButton);
+  promptBox.appendChild(cancelButton);
+
+  promptContainer.appendChild(promptBox);
+  document.body.appendChild(promptContainer);
+
+  cancelButton.addEventListener('click', closePrompt);
+
+  window.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+      closePrompt();
+    }
+  });
+
+  promptInput.focus();
+
+  return {promptContainer, promptBox, promptInput, submitButton, cancelButton};
+}
+
+function closePrompt() {
+  const promptContainer = document.querySelector('.prompt-container');
+  if (promptContainer) {
+    promptContainer.remove();
+  }
+}
+
 function addText() {
-  const text = prompt("Before adding text please click or tap somewhere on the board where you want to add your text: ");
-  const sanitizedText = DOMPurify.sanitize(text);
-
-  if (!sanitizedText || sanitizedText.trim().length === 0) {
-    alert('Please enter a valid text!');
-    return;
-  }
-
-  if (isTouchDevice()) {
-    textX = lastX - canvas.offsetLeft;
-    textY = lastY - canvas.offsetTop;
-  } else {
-    textX = lastX;
-    textY = lastY;
-  }
-
-  ctx.font = "25px Arial";
-  ctx.fillStyle = document.getElementById("color-picker").value;
+  const { promptContainer, promptBox, promptInput, submitButton, cancelButton } = openPrompt("Before adding text please click or tap somewhere on the board where you want to add your text: ");
   
-  const textWidth = ctx.measureText(sanitizedText).width;
-  const textHeight = parseInt(ctx.font);
+  function addTextCallback() {
+    const text = promptInput.value;
+    const sanitizedText = DOMPurify.sanitize(text);
 
-  if (textX + textWidth > canvas.width) {
-    textX = canvas.width - textWidth;
-  }
-  if (textY + textHeight > canvas.height) {
-    textY = canvas.height - textHeight;
+    if (!sanitizedText || sanitizedText.trim().length === 0) {
+      alert('Please enter a valid text!');
+      return;
+    }
+
+    if (isTouchDevice()) {
+      textX = lastX - canvas.offsetLeft;
+      textY = lastY - canvas.offsetTop;
+    } else {
+      textX = lastX;
+      textY = lastY;
+    }
+
+    ctx.font = "25px Arial";
+    ctx.fillStyle = document.getElementById("color-picker").value;
+
+    const textWidth = ctx.measureText(sanitizedText).width;
+    const textHeight = parseInt(ctx.font);
+
+    if (textX + textWidth > canvas.width) {
+      textX = canvas.width - textWidth;
+    }
+    if (textY + textHeight > canvas.height) {
+      textY = canvas.height - textHeight;
+    }
+
+    ctx.fillText(sanitizedText, textX, textY);
+    
+    closePrompt();
   }
 
-  ctx.fillText(sanitizedText, textX, textY);
+  submitButton.addEventListener('click', addTextCallback);
 }
 
 function isTouchDevice() {
@@ -138,9 +214,23 @@ canvas.addEventListener("click", function (event) {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    let radius = prompt("Enter the radius of the circle:");
-    drawCircle(x, y, radius);
-    isCircleDrawn = true
+    const { promptContainer, promptBox, promptInput, submitButton, cancelButton } = openPrompt("Enter the radius of the circle: ");
+    function addCircle() {
+      const radius = promptInput.value;
+      const sanitizedRadius = parseFloat(radius);
+
+      if (isNaN(sanitizedRadius)) {
+        alert('Please enter a valid radius!');
+        return;
+      }
+
+      drawCircle(x, y, sanitizedRadius);
+      isCircleDrawn = true;
+
+      closePrompt();
+    }
+
+    submitButton.addEventListener('click', addCircle);
   }
 });
 
